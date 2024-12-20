3f497fe843d06588cfb6f32f78dd65602e79a948
iam-ogomide <ogomidesamuel10@gmail.com>
2024-12-21 00:59:48 +0100

    6%

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,56 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import "../global.css";
+import { images } from '../constants'
+import CustomButton from '../components/CustomButton'
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl font-pblack'>OPen up</Text>
-      <StatusBar style='auto' />
-      <Link href="/home">Go to profie</Link>
-    </View>
-  )
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height:'100%'}}>
+        <View className='w-full h-full justify-center items-center px-4'>
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
+          />
+          <Image 
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode='contain'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-5xl text-white font-bold text-center'>Discover Endless{"\n"} possiblities with {' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+
+            <Image 
+              source={images.path}
+              className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+              resizeMode='contain'
+            />
+          </View>
+
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+            Where creativity meets innovation: embark on a journey{"\n"} of limitless exploration with Aora 
+          </Text>
+
+          <CustomButton />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  ) 
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center',
-    }
-})
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor:'#fff',
+//         alignItems:'center',
+//         justifyContent:'center',
+//     }
+// })
